53079efb5ecc39778ae8fe48a12841d57570a2fe
Jason McDonald <jasonmcd65@gmail.com>
2020-12-14 12:47:44 -0600

    gathers user input

---
+++ Develop/index.js
@@ -0,0 +1,38 @@
+const inquirer = require('inquirer');
+
+// array of questions for user
+const questions = [
+    ['input', 'What is the title of your project?', 'title'],
+    ['input', 'Please enter a description of your project.', 'description'],
+    ['input', 'How must a user install any dependecies needed?', 'install'],
+    ['input', 'How does the program need to be run?', 'run'],
+    ['list', 'Please choose a license for your project.','license'],
+    ['input', 'What are the guidelines for contribution?', 'guide'],
+    ['input', 'How can you go about testing the application?', 'test']
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+    inquirer.prompt([
+        { type: questions[0][0], message: questions[0][1], name: questions[0][2] }, //title
+        { type: questions[1][0], message: questions[1][1], name: questions[1][2] }, //description
+        { type: questions[2][0], message: questions[2][1], name: questions[2][2] }, //installation
+        { type: questions[3][0], message: questions[3][1], name: questions[3][2] }, //run
+
+        { type: questions[4][0], message: questions[4][1], name: questions[4][2], choices: ['hello', 'world']}, // license
+
+        { type: questions[5][0], message: questions[5][1], name: questions[5][2] }, // guidelines
+        { type: questions[6][0], message: questions[6][1], name: questions[6][2] }, // test
+
+    ]).then((response) => {
+        console.log(response)
+    });
+
+}
+
+// function call to initialize program
+init();
