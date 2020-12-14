2e8cecd4a881d618c60c9a7770d42d0404d8de59
Jason McDonald <jasonmcd65@gmail.com>
2020-12-14 16:19:39 -0600

    start of MD generation

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,32 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# # ${data.title} 
+
+  ## Description
+  ${data.description}
+  
+  ## License
+  ### This program is protected under the ${license} license
+  
+  ## Installation
+  You can install dependent packages by running:
+  \`\`\`
+  ${install}
+  \`\`\`
+  ## Running the program
+  To run the program, use the command:
+  \`\`\`
+  ${run}
+  \`\`\`
+  
+  ## Contribution Guidelines
+  ${guide}
+  
+  ## Testing Process
+  ${test}
+  
+  ## Questions
+  
 
 `;
 }
