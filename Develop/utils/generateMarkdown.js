9dff98023c8a48e480cd91d5ff02c51e082ea3f7
Jason McDonald <jasonmcd65@gmail.com>
2020-12-15 13:58:18 -0600

    layout for markdown file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,31 +1,46 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# # ${data.title} 
+  return `# ${data.title} 
 
+  # Table of Contents
+  1. [Description](#description)
+  2. [License](#license)
+  3. [Installation](#installation)
+  4. [Running the Program](#running-the-program)
+  5. [Contribution Guidelines](#contribution-guidelines)
+  6. [Testing Process](#testing-process)
+  7. [Questions](#questions)
+  
   ## Description
   ${data.description}
   
   ## License
-  ### This program is protected under the ${license} license
+  ${data.licenseBadge}
+
+  ### This program is protected under the ${data.license} license
+  ${data.licenseURL}
   
   ## Installation
   You can install dependent packages by running:
   \`\`\`
-  ${install}
+  ${data.install}
   \`\`\`
-  ## Running the program
+  ## Running the Program
   To run the program, use the command:
   \`\`\`
-  ${run}
+  ${data.run}
   \`\`\`
   
   ## Contribution Guidelines
-  ${guide}
+  ${data.guide}
   
   ## Testing Process
-  ${test}
+  ${data.test}
   
   ## Questions
+  Link to my Github: <https://github.com/${data.github}> 
+  
+  If you have any questions for me, please contact me at ${data.email}
   
 
 `;
